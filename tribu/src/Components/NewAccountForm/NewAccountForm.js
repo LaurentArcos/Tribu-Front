54f245f35db91ed17beb3ffa71c0b69cc3041eaa
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-16 12:06:51 +0200

    NewAccountForm component set up

---
+++ tribu/src/Components/NewAccountForm/NewAccountForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Imports locaux
+import '../../styles/styles.scss';
+
+function NewAccountForm() {
+  return (
+    <div className="newaccount">
+      New account Form
+    </div>
+  );
+}
+
+export default NewAccountForm;
