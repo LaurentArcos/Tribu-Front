d85ccf973585b5abb9c83fec64401d757c6330e6
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-16 14:28:27 +0200

    form set up with inputs using React-Hook-Form

--- tribu/src/Components/NewAccountForm/NewAccountForm.js
+++ tribu/src/Components/NewAccountForm/NewAccountForm.js
@@ -1,13 +1,34 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 // Imports locaux
 import '../../styles/styles.scss';
 
 function NewAccountForm() {
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm();
+
   return (
-    <div className="newaccount">
-      New account Form
-    </div>
+    <form
+      className="newaccount"
+      onSubmit={handleSubmit((data) => {
+        console.log(data);
+      })}
+    >
+      <input type="text" {...register('prénom')} />
+      <input type="text" {...register('nom')} />
+      <input type="text" {...register('ville')} />
+      <input type="email" {...register('email')} />
+      <input type="text" {...register('avatar')} />
+      <input type="text" {...register('description')} />
+      <input type="password" {...register('mot de passe')} />
+      <input type="password" {...register('confirmation mot de passe')} />
+      <input type="submit" />
+    </form>
   );
 }
 
